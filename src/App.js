a7d10c5314cf5fea0e01b7746d186339adacfced
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-02-17 14:06:07 -0800

    completed stretch goal, separated home and away components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 //TODO: STEP 1 - Import the useState hook.
 import React, { useState } from "react";
 import "./App.css";
+import Home from "./Home.js";
+import Away from "./Away.js";
 import BottomRow from "./BottomRow";
 
 function App() {
@@ -12,18 +14,9 @@ function App() {
     <div className="container">
       <section className="scoreboard">
         <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lions</h2>
-
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-
-            <div className="home__score">{homeScore}</div>
-          </div>
+          <Home homeScore = {homeScore} />
           <div className="timer">00:03</div>
-          <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">{awayScore}</div>
-          </div>
+          <Away awayScore = {awayScore} />
         </div>
         <BottomRow />
       </section>
